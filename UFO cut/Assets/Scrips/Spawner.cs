ec85425a579233062a5177d9e5dfefbef3397a71
beatrizramirez123 <plantitalinda6@gmail.com>
2024-03-13 09:16:33 -0300

    atualiçao celular desenhos e spawn

---
+++ UFO cut/Assets/Scrips/Spawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public float spawnRate = 2f;
+    float nextSpawn;
+
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            Vector2 randomSpawnPosition = new Vector2(Random.Range(-2.5f, 3), 6);
+            Instantiate(enemy, randomSpawnPosition, Quaternion.identity);
+        }
+    }
+}
