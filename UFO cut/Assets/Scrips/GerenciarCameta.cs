d37333f233af4bce2da80dfa5dd4f6e96e978bb8
beatrizramirez123 <plantitalinda6@gmail.com>
2024-03-19 10:41:33 -0300

    camera

---
+++ UFO cut/Assets/Scrips/GerenciarCameta.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerenciarCameta : MonoBehaviour
+{
+
+    public Camera cam;
+    private static float minX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
